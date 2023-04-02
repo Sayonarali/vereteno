35f04fee93964ed4a76c6e8c9da4e3a30e3c6d73
Sayonarali <elizaveta_shevchenko_00@inbox.ru>
2023-04-02 19:51:11 +0300

    feat: categories rest api

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CategoryController extends Controller
+{
+    public function index(): JsonResponse
+    {
+        $categories = Category::all();
+
+        return response()->json([
+            'categories' => $categories,
+        ],Response::HTTP_OK);
+    }
+
+    public function show(int $id): JsonResponse
+    {
+        $category = Category::find($id);
+
+        return response()->json([
+            'category' => $category,
+        ],Response::HTTP_OK);
+    }
+
+    public function create(Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'bail|required|string|unique:categories|max:255',
+            'type' => 'bail|nullable|string|max:255'
+        ]);
+
+        $category = new Category();
+        $category->name = $request->name;
+        if(!empty($request->type)){
+            $category->type = $request->type;
+        }
+
+        $category->save();
+
+        return response()->json([
+            'category' => $category,
+        ],Response::HTTP_CREATED);
+    }
+
+    public function update(int $id, Request $request): JsonResponse
+    {
+        $request->validate([
+            'name' => 'bail|nullable|string|unique:categories|max:255',
+            'type' => 'bail|nullable|string|max:255'
+        ]);
+
+        $category = Category::find($id);
+
+        if(!empty($request->name)){
+            $category->name = $request->name;
+        }
+        if(!empty($request->type)){
+            $category->type = $request->type;
+        }
+
+        return response()->json([
+            'category' => $category,
+        ],Response::HTTP_OK);
+    }
+
+    public function delete(int $id): JsonResponse
+    {
+        Category::destroy($id);
+
+        return response()->json([
+            'message' => 'deleted'
+        ], Response::HTTP_OK);
+    }
+}
