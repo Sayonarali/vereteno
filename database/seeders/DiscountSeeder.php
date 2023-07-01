d0160fc789956d29bf906c2b1541f8457d74fae0
Елизавета Шевченко <@shevchenko.ev.1>
2023-07-01 16:51:26 +0300

    feat: seeders

---
+++ database/seeders/DiscountSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Discount;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DiscountSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Discount::factory()
+            ->count(30)
+            ->create();
+    }
+}
