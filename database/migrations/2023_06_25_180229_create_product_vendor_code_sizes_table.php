b3589b8f52fae68972e7504cd148af1bef59f81a
Elizaveta Shevchenko <@shevchenko.ev.1>
2023-08-14 21:06:42 +0300

    feat: return migration sizes

---
+++ database/migrations/2023_06_25_180229_create_product_vendor_code_sizes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('product_vendor_code_sizes', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('product_vendor_code_id')->nullable()
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->foreignId('size_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+            $table->integer('quantity');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('product_vendor_code_sizes');
+    }
+};
